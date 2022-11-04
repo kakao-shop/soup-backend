b1af06b208cb58fb4542d8c08de57bb0337f0ae8
kim-yeji <2214yjgm@gmail.com>
2022-11-04 12:03:30 +0900

    chore: 프로젝트 세팅

---
+++ src/main/java/com/example/soup/SoupApplication.java
@@ -0,0 +1,13 @@
+package com.example.soup;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SoupApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SoupApplication.class, args);
+	}
+
+}
