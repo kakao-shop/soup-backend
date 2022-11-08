8b95be92064938bb129472578a2375dc43012509
kim-yeji <2214yjgm@gmail.com>
2022-11-08 14:21:48 +0900

    feat: 로그인 및 로그아웃 (#4)
    loginInterceptor를 통한 jwt access token 만료여부 확인

--- src/main/java/com/example/soup/common/advice/JWTExceptionHandler.java
+++ src/main/java/com/example/soup/common/advice/JWTExceptionHandler.java
@@ -1,9 +1,10 @@
 package com.example.soup.common.advice;
 
-import com.example.soup.common.exceptions.NoAccessTokenException;
-import com.example.soup.common.exceptions.NoRefreshTokenException;
 import com.example.soup.common.dto.BaseResponse;
 import com.example.soup.common.dto.ErrorCode;
+import com.example.soup.common.exceptions.InvalidTokenException;
+import com.example.soup.common.exceptions.NoAccessTokenException;
+import com.example.soup.common.exceptions.NoRefreshTokenException;
 import io.jsonwebtoken.ExpiredJwtException;
 import io.jsonwebtoken.JwtException;
 import org.springframework.http.ResponseEntity;
@@ -23,13 +24,18 @@ public class JWTExceptionHandler {
         return ResponseEntity.badRequest().body(new BaseResponse(ErrorCode.NoRefreshToken));
     }
 
-    @ExceptionHandler(ExpiredJwtException.class)
-    public ResponseEntity<BaseResponse> handleExpiredJwt(){
+    @ExceptionHandler({ExpiredJwtException.class,ExpiredJwtException.class})
+    public ResponseEntity<BaseResponse> handleExpiredJwt() {
         return ResponseEntity.badRequest().body(new BaseResponse(ErrorCode.ExpiredJwt));
     }
-    @ExceptionHandler(JwtException.class)
-    public ResponseEntity<BaseResponse> handleJwtValidation(){
+
+    @ExceptionHandler({JwtException.class, InvalidTokenException.class})
+    public ResponseEntity<BaseResponse> handleJwtValidation() {
         return ResponseEntity.badRequest().body(new BaseResponse(ErrorCode.JwtValidation));
     }
+
+
+
+
 
 }
