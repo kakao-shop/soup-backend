2a033254bdb08dd85788ae796752247a2ce48105
kim-yeji <2214yjgm@gmail.com>
2022-11-11 02:05:41 +0900

    feat: 검색 기능 개발 #10
    테마별 추천 더보기 기능을 위해 보여줄 상품 개수 늘리기, Paging 처리, 테마에 해당하는 모든 상품들을 합쳐서 정렬하도록 변경

--- src/main/java/com/example/soup/search/SearchController.java
+++ src/main/java/com/example/soup/search/SearchController.java
@@ -4,13 +4,15 @@ import com.example.soup.common.dto.BaseResponse;
 import com.example.soup.elasticsearch.Product;
 import lombok.RequiredArgsConstructor;
 import org.springframework.data.domain.Page;
-import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.PageImpl;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.domain.Sort;
+import org.springframework.data.web.PageableDefault;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.ArrayList;
+import java.util.Collections;
 import java.util.List;
 
 @RequestMapping("/search")
@@ -22,48 +24,32 @@ public class SearchController {
 
     // 상품 상세 페이지로 이동하는 링크 반환 API
     @GetMapping("/{productId}")
-    public ResponseEntity<BaseResponse> searchWebURL(@PathVariable("productId") String productId) {
-        String webURL = searchService.searchWebURL(productId);
+    public ResponseEntity<BaseResponse> searchByWebURL(@PathVariable("productId") String productId) {
+        String webURL = searchService.searchByWebURL(productId);
         return ResponseEntity.ok(new BaseResponse(200, "성공", webURL));
     }
 
     // 소카테고리 검색
     @GetMapping()
-    public ResponseEntity<BaseResponse> searchSubcat(@RequestParam(name = "category") String subcat,
-                                                     @RequestParam(defaultValue = "30") int limit,
-                                                     @RequestParam(defaultValue = "best-seller") String type,
-                                                     @RequestParam(defaultValue = "0") int page) {
-        Sort sort;
-        if (type.equals("low-price")) {
-            sort = Sort.by("price").ascending();
-        } else if (type.equals("high-price")) {
-            sort = Sort.by("price").descending();
-        } else {
-            sort = Sort.by("purchase").descending();
-        }
-        Pageable pageable = PageRequest.of(page, limit, sort);
-        Page<Product> result = searchService.searchSubcat(subcat, pageable);
+    public ResponseEntity<BaseResponse> searchBySubcat(@RequestParam(name = "category") String subcat,
+                                                       @PageableDefault(size = 10, sort = "purchase", direction = Sort.Direction.DESC) Pageable pageable) {
+        Page<Product> result = searchService.searchBySubcat(subcat, pageable);
         return ResponseEntity.ok(new BaseResponse(200, "성공", result));
     }
 
     // 테마별 상품 추천
     @GetMapping("/collections/{themeIdx}")
-    public ResponseEntity<BaseResponse> searchTheme(@PathVariable("themeIdx") Long themeIdx) {
+    public ResponseEntity<BaseResponse> searchTheme(@PathVariable("themeIdx") Long themeIdx,
+                                                    @PageableDefault(size = 10, sort = "purchase", direction = Sort.Direction.DESC) Pageable defaultPageable) {
         List<String> catList = searchService.findByThemeIdx(themeIdx);
-        List<Product> result = new ArrayList<>();
-        int cnt = catList.size();
-        int size = 10 / cnt;
-        for (int i = 0; i < cnt; i++) {
-            if (i == (cnt - 1)) {
-                size += 10 % cnt;
-            }
-            Pageable pageable = PageRequest.of(0, size, Sort.by("purchase").descending());
-            result.addAll(searchService.searchSubcat(catList.get(i), pageable).toList());
+        List<Product> prdList = new ArrayList<>();
+        for (int i = 0; i < catList.size(); i++) {
+            prdList.addAll(searchService.searchAllBySubcat(catList.get(i)));
         }
-        if (result.size() < 10) {
-            Pageable pageable = PageRequest.of(0, 10 - result.size(), Sort.by("purchase").descending());
-            result.addAll(searchService.searchAll(pageable).toList());
-        }
+        Collections.sort(prdList, Collections.reverseOrder());
+        final int start = (int) defaultPageable.getOffset();
+        final int end = Math.min((start + defaultPageable.getPageSize()), prdList.size());
+        final Page<Product> result = new PageImpl<>(prdList.subList(start, end), defaultPageable, prdList.size());
         return ResponseEntity.ok(new BaseResponse(200, "성공", result));
     }
 }
