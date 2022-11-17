d26e4eb49537b381dda33ae7831da20d66c18610
kim-yeji <2214yjgm@gmail.com>
2022-11-16 23:10:08 -0600

    fix: 서버 시간을 한국 시간으로 설정

--- src/main/java/com/kcs/soup/SoupApplication.java
+++ src/main/java/com/kcs/soup/SoupApplication.java
@@ -5,10 +5,20 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 import org.springframework.scheduling.annotation.EnableScheduling;
 
+import javax.annotation.PostConstruct;
+import java.util.TimeZone;
+
 @SpringBootApplication
 @EnableJpaRepositories(basePackages = "com.kcs.soup.api")
 @EnableScheduling
 public class SoupApplication {
+
+    @PostConstruct
+    void started() {
+        // timezone UTC 셋팅
+        TimeZone.setDefault(TimeZone.getTimeZone("Asia/Seoul"));
+    }
+
 
     public static void main(String[] args) {
         SpringApplication.run(SoupApplication.class, args);
