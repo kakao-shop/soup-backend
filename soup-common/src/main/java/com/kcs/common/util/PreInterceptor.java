316769a7aef03187cf9f349fc458881ffabfe123
kjh <aam41@naver.com>
2022-12-02 23:08:17 +0900

    feat: logback,fluentd logging test

--- soup-common/src/main/java/com/kcs/common/util/PreInterceptor.java
+++ soup-common/src/main/java/com/kcs/common/util/PreInterceptor.java
@@ -1,6 +1,9 @@
 package com.kcs.common.util;
 
 
+import lombok.extern.slf4j.Slf4j;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Component;
 import org.springframework.web.servlet.HandlerInterceptor;
 
@@ -8,10 +11,12 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
 @Component
+@Slf4j
 public class PreInterceptor implements HandlerInterceptor {
-
+    private final Logger logger = LoggerFactory.getLogger("LoggerController 의 로그");
     @Override
     public boolean preHandle(HttpServletRequest request, HttpServletResponse response, Object handler) throws Exception {
+        logger.info(request.getRequestURI());
         System.out.println(request.getRequestURI());
         return true;
     }
